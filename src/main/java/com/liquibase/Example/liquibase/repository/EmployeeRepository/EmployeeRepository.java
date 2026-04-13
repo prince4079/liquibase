3f48a45d02b56d4705423d61519cde23a2812323
Prince <prakashprince095@gmail.com>
2026-04-13 23:26:50 +0530

    user other feature is implemented

--- src/main/java/com/liquibase/Example/liquibase/repository/EmployeeRepository/EmployeeRepository.java
+++ src/main/java/com/liquibase/Example/liquibase/repository/EmployeeRepository/EmployeeRepository.java
@@ -1,8 +1,13 @@
 package com.liquibase.Example.liquibase.repository.EmployeeRepository;
 
 import com.liquibase.Example.liquibase.entity.Employee;
+import com.liquibase.Example.liquibase.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // Standard CRUD methods are built-in
+
 }
